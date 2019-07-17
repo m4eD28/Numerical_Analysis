f58b3ef3d7e91a6eaecaaf7f27f69738f52be3ba
m4eDtus <1418104@ed.tus.ac.jp>
2019-07-18 00:54:39 +0900

    厳密解間違ってませんか

---
+++ python/Logistic_kadai9.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+def logistic_analysitc(u0, r, K, T):
+    return K / (1 + (K/u0 - 1) * math.exp(r*T))
+
+def euler_logistic(u0, r, K, T, N, color='k'):
+    tau = T/N
+    tt = np.linspace(0, T, N+1)
+    uu = []
+    uu = np.append(uu, u0)
+    u, t= u0, 0
+    for i in range(N):
+        u_new = u + tau*r*u*(1 - u/K)
+        t = t + tau
+        uu = np.append(uu, u_new)
+        u = u_new
+
+    plt.plot(tt, uu, color=color)
+    plt.xlabel('t')
+    plt.ylabel('u(t)')
+    plt.title('u(t), u0 = %d, r = %d, K = %d' % (u0, r, K))
+    return uu
+
+def error(T, N):
+    uu = euler_logistic(1, 1, 10, T, N)
+    uu_analystic = logistic_analysitc(1, 1, 10, T)
+    error = abs(uu[N] - uu_analystic)
+    h = T/N
+    print('1 = %.10e' % uu_analystic)
+    print('2 = %.10e' % uu[N])
+    print('N = %d, h = %.e, |u(N) - u(T)| = %.10e' % (N, h, error))
+
+if __name__ == '__main__':
+    # (1) -------------------------------------
+    uu = euler_logistic(1, 1, 10, 8, 400, )
+    plt.show()
+    print('u(n) = %.10e' % (uu[400]))
+
+    # (2) ---------------------------------------
+    plt.subplot(3, 1, 1)
+    euler_logistic(5, -1, 7, 8, 400, )
+    plt.subplot(3, 1, 2)
+    euler_logistic(5, 0.5, 7, 8, 400, )
+    plt.subplot(3, 1, 3)
+    euler_logistic(5, 3, 7, 8, 400, )
+    plt.tight_layout()
+    plt.show()
+
+    plt.subplot(3, 1, 1)
+    euler_logistic(5, -1, 20, 8, 400, )
+    plt.subplot(3, 1, 2)
+    euler_logistic(5, 0.5, 20, 8, 400, )
+    plt.subplot(3, 1, 3)
+    euler_logistic(5, 3, 20, 8, 400, )
+    plt.tight_layout()
+    plt.show()
+
+    plt.subplot(3, 1, 1)
+    euler_logistic(30, -1, 7, 8, 400, )
+    plt.subplot(3, 1, 2)
+    euler_logistic(30, 0.5, 7, 8, 400, )
+    plt.subplot(3, 1, 3)
+    euler_logistic(30, 3, 7, 8, 400, )
+    plt.tight_layout()
+    plt.show()
+
+    plt.subplot(3, 1, 1)
+    euler_logistic(30, -1, 20, 8, 400, )
+    plt.subplot(3, 1, 2)
+    euler_logistic(30, 0.5, 20, 8, 400, )
+    plt.subplot(3, 1, 3)
+    euler_logistic(30, 3, 20, 8, 400, )
+    plt.tight_layout()
+    plt.show()
+
+    # (3) --------------------------------
+    error(8, 100)
+    error(8, 200)
+    error(8, 400)
+    error(8, 800)
+    error(8, 1600)
+
